5913e48da42402e877ce9dfa6aa584986faef197
lijiawei <ljw@live.jp>
2024-01-07 15:58:31 +0800

    feat: config

--- .umirc.js
+++ .umirc.js
@@ -2,5 +2,7 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   ssr: {},
+  dynamicImport: {},
   routes: require('./config/routes').map(({ path, component }) => ({ path, component })),
+  fastRefresh: {},
 });
